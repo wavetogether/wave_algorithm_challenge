c0f8b31b3fb823ceb2868b724b651b6eec94d0b6
Heedo Kim(Choco) on Macbook <devheedoo@gmail.com>
2020-01-21 20:28:25 +0900

    Improve: faster but heavier

--- 2020-01/21/choco.js
+++ 2020-01/21/choco.js
@@ -3,29 +3,33 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-  const rowsCount = board.length;
-  const columnsCount = board[0].length;
-  const prevBoard = board.map(row => row.slice());
+  const prevBoard = board.map(row => row.slice());  // DEEP COPY!
 
-  for (let i=0; i<rowsCount; i++) {
-    for (let j=0; j<columnsCount; j++) {
+  // i, j for iterating the board
+  for (let i=0; i<board.length; i++) {
+    for (let j=0; j<board[0].length; j++) {
+      // Count live neighbors of cell (i, j)
       let liveNeighborsCount = 0;
+      // k, l for iterating neighbors
       for (let k=i-1; k<=i+1; k++) {
         for (let l=j-1; l<=j+1; l++) {
           if (k === i && l === j) {
             // It's me
-          } else if ((k >= 0 && k < rowsCount) && ( l >= 0 && l < columnsCount)) {
+          } else if (prevBoard[k] !== undefined && prevBoard[k][l] !== undefined) {
             liveNeighborsCount += prevBoard[k][l];
           }
         }
       }
       if (prevBoard[i][j]) {
-        // dies by under-population
-        if (liveNeighborsCount < 2) board[i][j] = 0;
+        if (liveNeighborsCount < 2) {
+          // dies by under-population
+          board[i][j] = 0;
+        } else if (liveNeighborsCount > 3) {
+          // dies by over-population
+          board[i][j] = 0;
+        }
         // lives to the next generation
         // if (liveNeighborsCount === 2 || liveNeighborsCount === 3) {} // do nothing
-        // dies by over-population
-        if (liveNeighborsCount > 3) board[i][j] = 0;
       } else {
         // resurrects by reproduction
         if (liveNeighborsCount === 3) board[i][j] = 1;
