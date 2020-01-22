9383a853b902a3c9c571c5f323fad51e0eeb1902
eugene <sird176@gmail.com>
2020-01-22 14:03:32 +0900

    Update 2019-01-21

--- 2020-01/21/eugene.py
+++ 2020-01/21/eugene.py
@@ -4,16 +4,30 @@ class Solution(object):
         :type board: List[List[int]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
+        # for x in range(len(board)):
+        #     for y in range(len(board[0])):
+        #         live_cnt = sum(board[X][Y] % 2
+        #                        for X in range(max(0, x-1), min(x+2, len(board)))
+        #                        for Y in range(max(0, y-1), min(y+2, len(board[0])))
+        #                        if X != x or Y != y)
+        #         if board[x][y] == 0 and live_cnt == 3:
+        #             board[x][y] = 12
+        #         elif board[x][y] == 1 and live_cnt in (2, 3):
+        #             board[x][y] = 11
+        # for x in range(len(board)):
+        #     for y in range(len(board[0])):
+        #         board[x][y] /= 10
         for x in range(len(board)):
             for y in range(len(board[0])):
-                live_cnt = sum(board[X][Y] % 2
-                               for X in range(max(0, x-1), min(x+2, len(board)))
-                               for Y in range(max(0, y-1), min(y+2, len(board[0])))
-                               if X != x or Y != y)
-                if board[x][y] == 0 and live_cnt == 3:
-                    board[x][y] = 12
-                elif board[x][y] == 1 and live_cnt in (2, 3):
-                    board[x][y] = 11
+                if board[x][y] % 2 == 1:
+                    for X in range(max(0, x-1), min(x+2, len(board))):
+                        for Y in range(max(0, y-1), min(y+2, len(board[0]))):
+                            if X != x or Y != y:
+                                board[X][Y] += 2
         for x in range(len(board)):
             for y in range(len(board[0])):
-                board[x][y] /= 10
+                live_cnt = board[x][y] // 2
+                if board[x][y] % 2 == 0:
+                    board[x][y] = 1 if live_cnt == 3 else 0
+                else:
+                    board[x][y] = 1 if live_cnt in (2, 3) else 0
