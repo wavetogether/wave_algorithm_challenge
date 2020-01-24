83b1c1be9bb681100eab72f1e28d32e247ab7070
Sung Sil Kim (Tim) <mania@kaist.ac.kr>
2020-01-24 16:46:46 +0900

    Create tim.js

---
+++ 2020-01/24/tim.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for (let i = 0; i < s.length/2; i++) {
+      let head = s[i];
+      s[i] = s[s.length-1-i];
+      s[s.length-1-i] = head;
+    }
+  return s;
+};
