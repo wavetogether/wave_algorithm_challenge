5209e7f5b88994d62df69fab3a9c9bc786933257
Sung Sil Kim (Tim) <mania@kaist.ac.kr>
2020-01-26 01:17:44 +0900

    Update tim.js

--- 2020-01/25/tim.js
+++ 2020-01/25/tim.js
@@ -1,11 +1,4 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
