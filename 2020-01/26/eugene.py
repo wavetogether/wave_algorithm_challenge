af9cac985c263d0f358cc709c366dde19c218e8f
Seoungho Park <sird176@gmail.com>
2020-01-26 14:09:29 +0900

    Solve problem 2020-01-26 byy eugene
    Runtime: 36 ms, faster than 24.07% of Python online submissions for Maximum Depth of Binary Tree.
    Memory Usage: 14.6 MB, less than 64.20% of Python online submissions for Maximum Depth of Binary Tree.

---
+++ 2020-01/26/eugene.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1 if root else 0
