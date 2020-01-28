08a2a96c0ee551a044d5582aef7a8b421deb7dd8
eugene <sird176@gmail.com>
2020-01-28 12:45:53 +0900

    Solve problem 2020-01-28 by eugene
    Runtime: 20 ms, faster than 43.05% of Python online submissions for Climbing Stairs.
    Memory Usage: 11.7 MB, less than 42.19% of Python online submissions for Climbing Stairs.

---
+++ 2020-01/28/eugene.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 2
+        for _ in range(n-1):
+            a, b = b, a + b
+        return a
