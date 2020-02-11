c459361a01d6ab7cb00799d88c9b86e1e3dc248e
wavetogether-joy <59358867+wavetogether-joy@users.noreply.github.com>
2020-02-11 23:53:14 +0900

    Create joy.py

---
+++ 2020-02/10/joy.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr_node = head
+        prev_node = None
+        while curr_node != None:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        return prev_node
