72089856c1ea44b08117c1431924ba08c7acc3b7
Seoungho Park <sird176@gmail.com>
2020-02-19 00:42:52 +0900

    Solve problem 2020-02-17 by eugene

---
+++ 2020-02/17/eugene.py
@@ -0,0 +1,27 @@
+# Runtime: 28 ms, faster than 44.35% of Python online submissions for Merge Two Sorted Lists.
+# Memory Usage: 12 MB, less than 5.75% of Python online submissions for Merge Two Sorted Lists.
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return head.next
