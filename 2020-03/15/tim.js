15310c2a8063b24a56db8d79fbc469536e65cae1
Sung Sil Kim (Tim) <mania@kaist.ac.kr>
2020-03-22 01:41:31 +0900

    Update tim.js

--- 2020-03/15/tim.js
+++ 2020-03/15/tim.js
@@ -1,24 +1,11 @@
-// Runtime: 76 ms, faster than 35.52% of JavaScript online submissions for Reverse Bits.
-// Memory Usage: 36 MB, less than 66.67% of JavaScript online submissions for Reverse Bits.
+// Runtime: 72 ms, faster than 57.14% of JavaScript online submissions for Reverse Bits.
+// Memory Usage: 35.8 MB, less than 83.33% of JavaScript online submissions for Reverse Bits.
 
 /**
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-  // convert to decimal to bits
-  let binary = n.toString(2).toString();
-  // calculate number of missing leading zeros
-  let leadingZeros = 32 - binary.length;
-  
-  // concat leading zeros
-  for (let i=0; i < leadingZeros; i++) {
-    binary = '0' + binary;
-  }
-  
-  // reverse bits
-  binary = binary.split('').reverse().join('');
-  
-  // convert to decimal
-  return parseInt(binary, 2);
+  const bin = n.toString(2);
+  return parseInt(('0'.repeat(32 - bin.length) + bin).split('').reverse().join(''), 2);
 };
