c29594d3aaf610cccb543b04d35446b808758f35
Chris <56239299+wave-chris@users.noreply.github.com>
2020-07-08 14:46:40 +0900

    Create chris.go

---
+++ 2020-07-15/chris.go
@@ -0,0 +1,23 @@
+func minSteps(s string, t string) int {
+	sCharCount := map[rune]int{}
+	tCharCount := map[rune]int{}
+    
+	for _, char := range s {
+		sCharCount[char]++
+	}
+    
+	for _, char := range t {
+		tCharCount[char]++
+	}
+    
+	steps := 0
+	
+  for char := range sCharCount {
+		diff := sCharCount[char] - tCharCount[char]
+		if 0 < diff {
+			steps += diff
+		}
+	}
+  
+	return steps
+}
