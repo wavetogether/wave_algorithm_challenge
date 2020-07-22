dd8257b9e83262c13616d7bbfdf1dab024e7f862
Heedo Kim (Choco) <devheedoo@gmail.com>
2020-07-22 15:49:20 +0900

    Solve "Next Permutation"
    - Runtime: 80 ms, faster than 76.40% of JavaScript online submissions for Next Permutation.
    - Memory Usage: 37.5 MB, less than 12.19% of JavaScript online submissions for Next Permutation.

---
+++ 2020-07-29/choco.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    // If nums is short
+    if (nums.length === 1) return;
+    if (nums.length === 2) {
+        nums.reverse();
+        return;
+    }
+    
+    // Find first decreasing element
+    const numsLength = nums.length;
+    let lastDecreasingIndex = -1;
+    for (let i=numsLength-2; i>=0; i--) {
+        if (nums[i] < nums[i+1]) {
+            lastDecreasingIndex = i;
+            break;
+        }
+    }
+    
+    // If nums is decreasing, reverse
+    if (lastDecreasingIndex === -1) {
+        nums.reverse();
+        return;
+    }
+    
+    // Find bigger element
+    const firstDecreasingElement = nums[lastDecreasingIndex];
+    let lastMinimumBiggerIndex = lastDecreasingIndex+1;
+    for (let i=lastMinimumBiggerIndex; i<numsLength; i++) {
+        if (
+            nums[i] > firstDecreasingElement &&
+            nums[i] <= nums[lastMinimumBiggerIndex]
+        ) {
+            lastMinimumBiggerIndex = i;
+        }
+    }
+    
+    // Swap first decreaseing element and last minimum bigger element
+    nums[lastDecreasingIndex] = nums[lastMinimumBiggerIndex];
+    nums[lastMinimumBiggerIndex] = firstDecreasingElement;
+    
+    // Reverse after last decreasing index
+    for (let i=lastDecreasingIndex+1; i<numsLength; i++) {
+        if (i >= numsLength + lastDecreasingIndex - i) break;
+        const temp = nums[i];
+        nums[i] = nums[numsLength + lastDecreasingIndex - i];
+        nums[numsLength + lastDecreasingIndex - i] = temp;
+    }
+};
