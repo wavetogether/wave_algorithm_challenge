ced15d72336ad0afd14cf160c64b22bd59cbaca3
babo.neo <seirion@mensakorea.org>
2020-08-14 10:53:03 +0900

    Create neo.kt

---
+++ 2020-08-26/neo.kt
@@ -0,0 +1,27 @@
+class Solution {
+    private val `🍺🍺🍺` = ArrayList<List<Int>>()
+    fun combinationSum(`🦑`: IntArray, `🍎`: Int): List<List<Int>> {
+        `🦑`.sort()
+        `🐶`(`🦑`, ArrayList(), 0, 0, `🍎`)
+        return `🍺🍺🍺`
+    }
+
+    private tailrec fun `🐶`(`🦑`: IntArray, `🐇🐇`  : ArrayList<Int>, `👉👈🏻`: Int, `🍓🍓🍓`: Int, `🍎`: Int) {
+        if (`🍓🍓🍓` == `🍎`) {
+            `🍺🍺🍺`.add(ArrayList(`🐇🐇`))
+            return
+        }
+
+        if (`🍓🍓🍓` > `🍎` || `👉👈🏻` > `🦑`.lastIndex) {
+            return
+        }
+
+        // with current one
+        `🐇🐇`.add(`🦑`[`👉👈🏻`])
+        `🐶`(`🦑`, `🐇🐇`, `👉👈🏻`, `🍓🍓🍓` + `🦑`[`👉👈🏻`], `🍎`)
+        `🐇🐇`.removeAt(`🐇🐇`.lastIndex)
+
+        // with next one
+        `🐶`(`🦑`, `🐇🐇`, `👉👈🏻` + 1, `🍓🍓🍓`, `🍎`)
+    }
+}
