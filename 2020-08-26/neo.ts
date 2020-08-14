dc2b3f633b4a747a7894c0a91cfe44f170d30f8c
babo.neo <seirion@mensakorea.org>
2020-08-15 01:18:48 +0900

    Update neo.ts

--- 2020-08-26/neo.ts
+++ 2020-08-26/neo.ts
@@ -1,3 +1,8 @@
+function combinationSum(candidates: number[], target: number): number[][] {
+	const out: number[][] = [];
+	go(out, candidates, [], 0, 0, target);
+	return out;
+}
 function go(out: number[][], candidates: number[], current: number[], sum: number, index: number, target: number) {
 	if (sum == target) {
 		out.push(current.slice());
