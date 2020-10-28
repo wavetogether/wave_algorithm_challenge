d4388ed1270bf5c0a5572ff2d7f515fcdbd0dc7a
babo.neo <seirion@mensakorea.org>
2020-10-28 13:03:12 +0900

    Update neo.kt

--- 2020-10-14/neo.kt
+++ 2020-10-14/neo.kt
@@ -3,11 +3,7 @@ import java.util.*
 class Solution {
     fun largestNumber(nums: IntArray): String {
         return nums.map { it.toString() }
-                .sortedWith(object: Comparator<String> {
-                    override fun compare(a: String, b: String): Int {
-                        return (b + a).compareTo(a + b)
-                    }
-                })
+                .sortedWith { (a, b) -> (b + a).compareTo(a + b) }
                 .dropWhile { it == "0" }
                 .let {
                     if (it.isNotEmpty()) it.joinToString("")
