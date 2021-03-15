b7de7c1eb4070132e728aca6b0138db3ecb71b3b
Seoungho Park (eugene) <eugene@wavetogether.com>
2021-03-16 01:19:03 +0900

    Create eugene.py

---
+++ 2021-03-17/eugene.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return [1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851][n]
