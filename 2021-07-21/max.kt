77573bed798798c1dfadd36db9dc9ef410a12f10
max lee (Max) <pj4316@naver.com>
2021-07-15 13:48:05 +0900

    Update max.kt

--- 2021-07-21/max.kt
+++ 2021-07-21/max.kt
@@ -1,14 +1,14 @@
+// Runtime: 1576 ms, faster than 40.00% of Kotlin online submissions for Heaters.
+// Memory Usage: 39.3 MB, less than 100.00% of Kotlin online submissions for Heaters.
+
+import kotlin.math.abs
+
 class Solution {
     fun findRadius(houses: IntArray, heaters: IntArray): Int {
-        val houseMinRadius = houses.clone().map { house ->
-            heaters.fold(Int.MAX_VALUE) { min, heater ->
-                val range = if (house - heater > 0) house - heater else heater - house
-                minOf(min, range)
-            }
+        return houses.fold(Int.MIN_VALUE) { max, house ->
+            maxOf(max, heaters.fold(Int.MAX_VALUE) { min, heater ->
+                minOf(min, abs(house - heater))
+            })
         }
-
-        return houseMinRadius.fold(Int.MIN_VALUE) { max, it ->
-            maxOf(max, it)
-        };
     }
 }
