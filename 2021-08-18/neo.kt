31c80d29a239abd353a908b7f32953234d5aca00
babo.neo <seirion@mensakorea.org>
2021-08-26 11:56:45 +0900

    Update neo.kt

--- 2021-08-18/neo.kt
+++ 2021-08-18/neo.kt
@@ -1,5 +1,24 @@
 class Solution {
     fun maxNumberOfFamilies(n: Int, reservedSeats: Array<IntArray>): Int {
+        val m = TreeMap<Int, Int>() // [row, col] // col: bit mask
+        reservedSeats.forEach { (row, col) ->
+            m[row] = (m[row] ?: 1023) and (1 shl (col - 1)).inv() // to 0-based index
+        }
+        return m.values.map(this::calc).sum() + (n - m.size) * 2
+    }
+
+    private fun calc(v: Int) = when {
+        v and 510 == 510 -> 2 // left & right (011 1111 110)
+        v and 480 == 480 -> 1 // (011 1100 000)
+        v and 30 == 30 -> 1 // (000 0011 110)
+        v and 120 == 120 -> 1 // (000 1111 000)
+        else -> 0
+    }
+}
+
+
+class Solution {
+    fun maxNumberOfFamilies(n: Int, reservedSeats: Array<IntArray>): Int {
         val m = TreeMap<Int, TreeSet<Int>>() // [row, col]
         reservedSeats.forEach { (row, col) ->
             if (!m.containsKey(row)) {
