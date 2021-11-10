f90edba1dbe594564b06b941688b4159dd5953a1
Pandi0010 <92356058+Pandi0010@users.noreply.github.com>
2021-11-10 16:25:47 +0900

    Update pandi.js

--- 2021-11-10/pandi.js
+++ 2021-11-10/pandi.js
@@ -1,14 +1,11 @@
-
 /**
  * @param {number[]} people
  * @param {number} limit
  * @return {number}
  */
  var numRescueBoats = function(people, limit) {
-     let result = 0; //bort
-
      people.sort((a, b) => b - a);
-     let wegith = 0; // 
+     let i = 0; // 
      let len = people.length;
 
      for (; i < len; i++) {
