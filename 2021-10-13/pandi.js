8177e3e3c98e4dfa610a31164663c2eb4fe567f9
Pandi <pandi@wavetogether.com>
2021-11-17 17:12:18 +0900

    jump-game

---
+++ 2021-10-13/pandi.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = function(text1, text2) {
+    const T1 = text1.length;
+    const T2 = text2.length;
+
+    for ( i = 1; i <= T1; i++) {
+
+        for (j = 1; j <= T2; j++) {
+            if(text1.length === text2.length){
+
+            }
+
+        }
+    }
+
+}
+
+
+
+
